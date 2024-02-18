00a34d0ae39b083f36437e59ca82ff8e3f6d2b46
Bern Dioso <berndione03@gmail.com>
2024-02-18 22:44:11 +0800

    commit

--- Activity1/obj/Release/net8.0/Activity1.AssemblyInfo.cs
+++ Activity1/obj/Release/net8.0/Activity1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Activity1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb43ddb3b27c7426fb9bede1730bd39d83863d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Activity1")]
--- Activity2/obj/Debug/net8.0/Activity2.AssemblyInfo.cs
+++ Activity2/obj/Debug/net8.0/Activity2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Activity2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb43ddb3b27c7426fb9bede1730bd39d83863d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Activity2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Activity2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
